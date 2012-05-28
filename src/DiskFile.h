00af29eb8966ee2b57bd54ee816b8ade90783c50
Jon Tomas Gretarsson <jon.gretarsson@gmail.com>
2012-05-28 17:25:08 -0400

    Implementing reading from disk

--- src/DiskFile.h
+++ src/DiskFile.h
@@ -1,19 +1,25 @@
 #ifndef __DISK_FILE_H__
 #define __DISK_FILE_H__
 
+#include <fstream>
+
 #include <Interfaces/FileInterface.h>
 
 using namespace std;
 
 class DiskFile : public FileInterface {
   public:
-    DiskFile(const char* path, const char* mode);
+    DiskFile(const char* path);
     virtual ~DiskFile();
 
     bool Open();
     bool ReadLine(string& line);
     bool eof();
     bool Close();
+
+  private:
+    string filename;
+    ifstream file;
 };
 
 #endif
